f4ef5d6316fb7b27471554c04de7a4a4bb78fc1b
hansione <87800381+hansione@users.noreply.github.com>
2022-03-02 17:30:01 +0900

    Add NPC ? Button

--- Assets/Scripts/NPC/NPC.cs
+++ Assets/Scripts/NPC/NPC.cs
@@ -1,10 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class NPC : MonoBehaviour
 {
     public Transform player;
+
+    public GameObject image;
+
+    private void Update()
+    {
+        image.transform.position = Camera.main.WorldToScreenPoint(transform.position + Vector3.up * 4.5f + Vector3.right * 0.2f);
+
+        if (Vector3.Distance(player.position, transform.position) < 5f)
+        {
+            image.SetActive(true);
+        }
+        else
+        {
+            image.SetActive(false);
+        }
+    }
 
     private void OnMouseDown()
     {
